2bba88a53e91c9054f504e67420437368abe1b87
morpha <v21.leonardo@gmail.com>
2022-05-14 16:32:52 -0300

    fixed some minor issues, commented code and increased (not much) readability

--- imgprocess.go
+++ imgprocess.go
@@ -10,14 +10,16 @@ import (
 )
 
 func main() {
-
+	//imports image
 	imageFile, err := os.Open(os.Args[1])
 	defer imageFile.Close()
 	checkError(err)
 
+	//prepares image for manipulation
 	img, _, err := image.Decode(imageFile)
 	checkError(err)
 
+	//creates a gray image with tiny black stripes
 	grayImg := image.NewGray(img.Bounds())
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x += 2 {
@@ -25,6 +27,7 @@ func main() {
 		}
 	}
 
+	//I'm trying to create a colored image with black stripes, but I'm kinda failing
 	imagem := image.NewRGBA(img.Bounds())
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X + 1; x < img.Bounds().Max.X; x += 2 {
@@ -35,14 +38,17 @@ func main() {
 		}
 	}
 
+	//exports the gray image to the 1newimage.png file
 	f, err := os.Create("1newimage.png")
 	checkError(err)
-	f2, err := os.Create("2newimage.png")
-	checkError(err)
+	checkError(png.Encode(f, grayImg))
+
 	defer f.Close()
 
-	checkError(png.Encode(f, grayImg))
-	checkError(png.Encode(f2, imagem))
+	//exports the colored image to the 2newimage.png file
+	f, err = os.Create("2newimage.png")
+	checkError(err)
+	checkError(png.Encode(f, imagem))
 }
 
 func checkError(err error) {
